81961997a736b59b0d8c5a19140f5df5b4d46060
0xAndrewBlack <andras@fekete.io>
2023-05-12 22:44:05 +0200

    ✨ feat: Updated pages.

---
+++ src/pages/about.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>PWA Kotprog 2023 - Chillzone</title>
+      </Head>
+      <h1>About</h1>
+      <Link href="/">Home</Link>
+    </>
+  );
+}
