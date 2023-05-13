a640530bf2607c8c570b81c23c2cb9a0a4f6cc91
0xAndrewBlack <andras@fekete.io>
2023-05-13 19:35:39 +0200

    ✨ feat: Audio.

--- src/components/Relax.tsx
+++ src/components/Relax.tsx
@@ -1,8 +1,24 @@
 import Link from "next/link";
-import { useEffect, useState } from "react";
+
+import { useEffect, useRef, useState } from "react";
 
 export default function Relax({ quotes }: any) {
+  const [isPlaying, setIsPlaying] = useState(false);
   const [randomQuote, setRandomQuote] = useState<any>(null);
+
+  const audioRef = useRef<HTMLAudioElement>(null);
+
+  const togglePlay = () => {
+    if (audioRef.current && audioRef.current.paused) {
+      audioRef.current.play();
+
+      setIsPlaying(true);
+    } else {
+      audioRef.current?.pause();
+
+      setIsPlaying(false);
+    }
+  };
 
   useEffect(() => {
     const getRandomQuote = () => {
@@ -24,7 +40,15 @@ export default function Relax({ quotes }: any) {
     <>
       <div className="flex flex-col items-center justify-center min-h-screen">
         <main className="flex flex-col items-center justify-center flex-1 px-20 text-center">
-          <button className="bg-lime-500 hover:bg-lime-600 text-white font-bold py-2 px-4 rounded-full">Play ambiance</button>
+          <audio ref={audioRef} src="/static/music/ambiance.mp3" />
+
+          <button className="bg-lime-500 hover:bg-lime-600 text-white font-bold py-2 px-4 rounded-full" onClick={togglePlay}>
+            {isPlaying ? "Pause" : "Play"} ambiance
+          </button>
+
+          {/* <audio ref={audioRef} src="/static/music/ambiance.mp3" /> */}
+          {/* <button onClick={togglePlay}>{audioRef.current && audioRef.current.paused ? "Play" : "Pause"}</button> */}
+
           <div className="mt-20">
             {randomQuote && (
               <p className="flex justify-center flex-1 px-20 text-center">
